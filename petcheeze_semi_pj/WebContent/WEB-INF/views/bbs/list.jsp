e74527a6e1e0e45e7073c6b8f416488cf3f2bcbf
user1 <user1@user1-PC>
2017-10-27 16:20:03 +0900

    com.bbs 작업

---
+++ petcheeze_semi_pj/WebContent/WEB-INF/views/bbs/list.jsp
@@ -0,0 +1,122 @@
+﻿<%@ page contentType="text/html; charset=UTF-8" %>
+<%@ page trimDirectiveWhitespaces="true" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<%
+   String cp = request.getContextPath();
+%>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>PetCheeze</title>
+
+<link rel="stylesheet" href="<%=cp%>/resource/css/style.css" type="text/css">
+<link rel="stylesheet" href="<%=cp%>/resource/css/layout.css" type="text/css">
+<link rel="stylesheet" href="<%=cp%>/resource/jquery/css/smoothness/jquery-ui.min.css" type="text/css">
+
+<script type="text/javascript" src="<%=cp%>/resource/js/util.js"></script>
+<script type="text/javascript" src="<%=cp%>/resource/jquery/js/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+	function searchList() {
+		var f=document.searchForm;
+		f.action="<%=cp%>/";
+		f.submit();
+	}
+</script>
+</head>
+<body>
+
+<div class="header">
+    <jsp:include page="/WEB-INF/views/layout/header.jsp"></jsp:include>
+</div>
+	
+	
+<div class="container">
+    <div class="body-container" style="width: 1060px; border: 1px solid #ffeab3; border-top:3px solid #ffd700;  padding: 20px 40px;">
+        <div class="body-title">
+            <h3><span style="font-family: Webdings"></span> 공지사항&nbsp;&amp;&nbsp;이벤트</h3>
+            <span style="font-family: sans-serif; font-size: 18px"></span>&nbsp;ㅣ&nbsp;&nbsp;펫치즈의 소식과 이벤트를 소개합니다.
+        </div>
+        
+        <div>
+			<table style="width: 100%; margin: 15px 0px 5px; border-spacing: 0px;">
+				<tr height="35">
+				
+					<td align="left" width="50%">
+											
+				    </td>
+				    
+				    <td align="right" width="50%">
+				          <form name="searchForm" action="" method="post">
+				              <select name="searchKey" class="selectField">
+				                  <option value="subject">제목</option>
+				                  <option value="userName">작성자</option>
+				                  <option value="content">내용</option>
+				                  <option value="created">등록일</option>
+				            </select>
+				            <input type="text" name="searchValue" class="boxTF">
+				            <button type="button" class="btn" onclick="searchList()" 
+				            style="border:1px solid #e7e8ea; height : 32px; text-align:center; width : 32px;">검색</button>
+				        </form>
+				     </td>
+			     
+			   </tr>
+			</table>
+			
+			<table style="width: 100%; margin: 0px auto; border-spacing: 0px; border-collapse: collapse;">
+			  <tr align="center" bgcolor="#eeeeee" height="35" style="border-top: 1px solid #cccccc; border-bottom: 1px solid #cccccc;"> 
+			      <th width="60" style="color: #787878;">번호</th>
+			      <th style="color: #787878;">제목</th>
+			      <th width="100" style="color: #787878;">작성자</th>
+			      <th width="80" style="color: #787878;">작성일</th>
+			      <th width="60" style="color: #787878;">조회수</th>
+			  </tr>
+			 
+			  <tr align="center" bgcolor="#ffffff" height="35" style="border-bottom: 1px solid #cccccc;"> 
+			      <td>1</td>
+			      <td align="left" style="padding-left: 10px;">
+			           <a href="#">제목입니다...</a>
+			      </td>
+			      <td>스프링</td>
+			      <td>2010-10-10</td>
+			      <td>1</td>
+			  </tr>
+
+			</table>
+			 
+			<table style="width: 100%; margin: 0px auto; border-spacing: 0px;">
+			   <tr height="35">
+				<td align="center">
+			        1 2 3
+				</td>
+			   </tr>
+			</table>
+			
+			<table style="width: 100%; margin: 10px auto; border-spacing: 10px;">
+					   
+			<tr height="40">
+				<td align="right" width="100">
+					<button type="button" class="btn" onclick="javascript:location.href='<%=cp%>/';">새로고침</button>
+				</td>
+			
+				<td align="left" width="100">
+					<button type="button" class="btn" onclick="javascript:location.href='<%=cp%>/bbs/created.do';">글쓰기</button>
+				</td>
+					</tr>
+			</table>
+			
+        </div>
+
+    </div>
+</div>
+
+<div class="footer">
+    <jsp:include page="/WEB-INF/views/layout/footer.jsp"></jsp:include>
+</div>
+
+<script type="text/javascript" src="<%=cp%>/resource/jquery/js/jquery-ui.min.js"></script>
+<script type="text/javascript" src="<%=cp%>/resource/jquery/js/jquery.ui.datepicker-ko.js"></script>
+</body>
+</html>
---
+++ petcheeze_semi_pj/src/com/bbs/BoardServlet.java
@@ -0,0 +1,303 @@
+package com.bbs;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.util.Iterator;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.member.SessionInfo;
+import com.util.MyServlet;
+import com.util.MyUtil;
+
+
+@WebServlet("/bbs/*")
+public class BoardServlet extends MyServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void process(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		
+		String uri=req.getRequestURI();
+		String cp=req.getContextPath();
+		
+		HttpSession session=req.getSession();
+		SessionInfo info=(SessionInfo)session.getAttribute("member");
+		if(info==null) { // 로그인되지 않은 경우
+			resp.sendRedirect(cp+"/member/login.do");
+			return;
+		}
+		
+		// uri에 따른 작업 구분
+		if(uri.indexOf("list.do")!=-1) {
+			list(req, resp);
+		} else if(uri.indexOf("created.do")!=-1) {
+			createdForm(req, resp);
+		} else if(uri.indexOf("created_ok.do")!=-1) {
+			createdSubmit(req, resp);
+		} else if(uri.indexOf("article.do")!=-1) {
+			article(req, resp);
+		} else if(uri.indexOf("update.do")!=-1) {
+			updateForm(req, resp);
+		} else if(uri.indexOf("update_ok.do")!=-1) {
+			updateSubmit(req, resp);
+		} else if(uri.indexOf("delete.do")!=-1) {
+			delete(req, resp);
+		}
+	}
+
+	private void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 게시물 리스트
+		String cp = req.getContextPath();
+
+		BoardDAO dao = new BoardDAO();
+		MyUtil util = new MyUtil();
+	
+		String page=req.getParameter("page");
+		int current_page=1;
+		if(page!=null)
+			current_page=Integer.parseInt(page);
+		
+		// 검색
+		String searchKey=req.getParameter("searchKey");
+		String searchValue=req.getParameter("searchValue");
+		if(searchKey==null) {
+			searchKey="subject";
+			searchValue="";
+		}
+		// GET 방식인 경우 디코딩
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			searchValue=URLDecoder.decode(searchValue, "utf-8");
+		}
+		
+		// 전체 데이터 개수
+		int dataCount;
+		if(searchValue.length()==0)
+			dataCount=dao.dataCount();
+		else
+			dataCount=dao.dataCount(searchKey, searchValue);
+		
+		// 전체 페이지 수
+		int rows=10;
+		int total_page=util.pageCount(rows, dataCount);
+		
+		if(current_page>total_page)
+			current_page=total_page;
+		
+		// 게시물 가져올 시작과 끝
+		int start=(current_page-1)*rows+1;
+		int end=current_page*rows;
+		
+		// 게시물 가져오기
+		List<BoardDTO> list=null;
+		if(searchValue.length()==0)
+			list=dao.listBoard(start, end);
+		else
+			list=dao.listBoard(start, end, searchKey, searchValue);
+		
+		// 리스트 글번호 만들기
+		int listNum, n=0;
+		Iterator<BoardDTO>it=list.iterator();
+		while(it.hasNext()) {
+			BoardDTO dto=it.next();
+			listNum=dataCount-(start+n-1);
+			dto.setListNum(listNum);
+			n++;
+		}
+		
+		String query="";
+		if(searchValue.length()!=0) {
+			// 검색인 경우 검색값 인코딩
+			searchValue=URLEncoder.encode(searchValue, "utf-8");
+			query="searchKey="+searchKey+
+					 "&searchValue="+searchValue;
+		}
+		
+		// 페이징 처리
+		String listUrl=cp+"/bbs/list.do";
+		String articleUrl=cp+"/bbs/article.do?page="+current_page;
+		if(query.length()!=0) {
+			listUrl+="?"+query;
+			articleUrl+="&"+query;
+		}
+		
+		String paging=util.paging(current_page, total_page, listUrl);
+		
+		// 포워딩할 JSP로 넘길 속성
+		req.setAttribute("list", list);
+		req.setAttribute("page", current_page);
+		req.setAttribute("total_page", total_page);
+		req.setAttribute("dataCount", dataCount);
+		req.setAttribute("articleUrl", articleUrl);
+		req.setAttribute("paging", paging);
+		
+		// JSP로 포워딩
+		String path="/WEB-INF/views/bbs/list.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void createdForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글쓰기 폼
+		req.setAttribute("mode", "created");
+		String path="/WEB-INF/views/bbs/created.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void createdSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글 저장
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		BoardDAO dao = new BoardDAO();
+		BoardDTO dto=new BoardDTO();
+		
+		// userId는 세션에 저장된 정보
+		dto.setUserId(info.getUserId());
+		
+		// 파라미터
+		dto.setSubject(req.getParameter("subject"));
+		dto.setContent(req.getParameter("content"));
+		
+		dao.insertBoard(dto);
+		
+		resp.sendRedirect(cp+"/bbs/list.do");
+	}
+
+	private void article(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 글보기
+		String cp = req.getContextPath();
+		BoardDAO dao = new BoardDAO();
+		MyUtil myUtil = new MyUtil();
+	
+		int num=Integer.parseInt(req.getParameter("num"));
+		String page=req.getParameter("page");
+		String searchKey=req.getParameter("searchKey");
+		String searchValue=req.getParameter("searchValue");
+		if(searchKey==null) {
+			searchKey="subject";
+			searchValue="";
+		}
+		
+		searchValue=URLDecoder.decode(searchValue, "utf-8");
+		
+		// 조회수 증가
+		dao.updateHitCount(num);
+		
+		// 게시물 가져오기
+		BoardDTO dto=dao.readBoard(num);
+		if(dto==null) { // 게시물이 없으면 다시 리스트로
+			resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+			return;
+		}
+		dto.setContent(myUtil.htmlSymbols(dto.getContent()));
+		
+		// 이전글 다음글
+		BoardDTO preReadDto=dao.preReadBoard(dto.getNum(), searchKey, searchValue);
+		BoardDTO nextReadDto=dao.nextReadBoard(dto.getNum(), searchKey, searchValue);
+		
+		// 게시물 공감 개수
+		int countLikeBoard = dao.countLikeBoard(num);
+		
+		// 리스트나 이전글/다음글에서 사용할 파라미터
+		String query="page="+page;
+		if(searchValue.length()!=0) {
+			query+="&searchKey="+searchKey
+					+"&searchValue="+URLEncoder.encode(searchValue, "utf-8");
+		}
+		
+		// JSP로 전달할 속성
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("query", query);
+		req.setAttribute("preReadDto", preReadDto);
+		req.setAttribute("nextReadDto", nextReadDto);
+		req.setAttribute("countLikeBoard", countLikeBoard);
+		
+		// 포워딩
+		String path="/WEB-INF/views/bbs/article.jsp";
+		forward(req, resp, path);
+	}
+	
+	private void updateForm(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 폼
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		BoardDAO dao = new BoardDAO();
+	
+		String page=req.getParameter("page");
+		int num=Integer.parseInt(	req.getParameter("num"));
+		BoardDTO dto=dao.readBoard(num);
+		
+		if(dto==null) {
+			resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+			return;
+		}
+		
+		// 게시물을 올린 사용자가 아니면
+		if(! dto.getUserId().equals(info.getUserId())) {
+			resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+			return;
+		}
+		
+		req.setAttribute("dto", dto);
+		req.setAttribute("page", page);
+		req.setAttribute("mode", "update");
+		
+		String path="/WEB-INF/views/bbs/created.jsp";
+		forward(req, resp, path);
+	}
+
+	private void updateSubmit(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 수정 완료
+		String cp = req.getContextPath();
+		BoardDAO dao = new BoardDAO();
+		HttpSession session=req.getSession();
+		SessionInfo info=(SessionInfo)session.getAttribute("member");
+	
+		String page=req.getParameter("page");
+		
+		if(req.getMethod().equalsIgnoreCase("GET")) {
+			resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+			return;
+		}
+		
+		BoardDTO dto=new BoardDTO();
+		dto.setNum(Integer.parseInt(req.getParameter("num")));
+		dto.setSubject(req.getParameter("subject"));
+		dto.setContent(req.getParameter("content"));
+		
+		dao.updateBoard(dto, info.getUserId());
+		
+		resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+	}
+
+	private void delete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 삭제
+		String cp = req.getContextPath();
+
+		HttpSession session = req.getSession();
+		SessionInfo info = (SessionInfo) session.getAttribute("member");
+
+		BoardDAO dao = new BoardDAO();
+	
+		String page=req.getParameter("page");
+		int num=Integer.parseInt(req.getParameter("num"));
+		
+		// bbsReply 테이블은 ON DELETE CASCADE 옵션으로 bbs 테이블의 데이터가 지워지면 자동 지워짐
+		dao.deleteBoard(num, info.getUserId());
+		resp.sendRedirect(cp+"/bbs/list.do?page="+page);
+	}
+}
